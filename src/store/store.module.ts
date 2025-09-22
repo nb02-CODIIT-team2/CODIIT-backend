ba00852e7db160d78af524a2d444e756a8aaf58a
jinsol kim <jinsol032360@gmail.com>
2025-09-22 10:49:42 +0900

    fix: StoreRepository 추가

--- src/store/store.module.ts
+++ src/store/store.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { StoreService } from './store.service';
 import { StoreController } from './store.controller';
+import { StoreRepository } from './store.repository';
 import { PrismaModule } from '../prisma/prisma.module';
 
 @Module({
   imports: [PrismaModule],
-  providers: [StoreService],
+  providers: [StoreService, StoreRepository],
   controllers: [StoreController],
 })
 export class StoreModule {}
